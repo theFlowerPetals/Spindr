d73da1ce79d72fe3c612ad3d358aa675ea3ea5f4
Dhruv Patel <dhruvp95@gmail.com>
2017-10-18 18:23:20 -0700

    fix user routes, create icebreaker and match table routes/ctrlers

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -3,6 +3,7 @@ const { User } = require('../../db/models/dataModels');
 module.exports = {
   //Create new user on new account login
   createUser: (req, res) => {
+    console.log("this is user req: ", req.body)
     User.create({
       name: req.body.name,
       email: req.body.email,
@@ -12,7 +13,15 @@ module.exports = {
     .then((user) => {
       res.status(201).send(user);
     })
-    .catch(err => res.status(404).send(err))
+    .catch(err => res.status(404).send(err.message))
+  },
+  //Grab all users from db
+  getAllUsers: (req, res) => {
+    User.findAll({})
+    .then((users) => {
+      res.status(200).send(users);
+    })
+    .catch(err => res.status(404).send(err));
   },
   //Grab user from db using email
   getUserByEmail: (req, res) => {
@@ -30,12 +39,34 @@ module.exports = {
   getUserById: (req, res) => {
     User.findOne({
       where: {
-        email: req.params.id
+        id: req.params.id
       }
     })
     .then((user) => {
+      console.log('found user: ', user)
       res.status(200).send(user);
     })
     .catch(err => res.status(404).send(err));
   },
+  //add interests
+  addInterests: (req, res) => {
+    console.log('this is the req: ', req.body)
+    User.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then((user) => {
+      user.interests.push(req.body.interests)
+      user.update({
+        interests: user.interests
+      })
+    })
+      .then((updated) => {
+        console.log('successfully updated', updated);
+        res.status(202).send(updated);
+      })
+      .catch(err => res.status(404).send(err.message))
+    .catch(err => res.status(404).send(err.message));
+  }
 }
