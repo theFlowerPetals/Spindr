417e983a6553633a7f91c2030f1b4c0aa2da9ef7
Alex Ko <alexkosj@gmail.com>
2017-10-25 22:51:25 -0700

    set up flask

--- server/roomGen/py_server.py
+++ server/roomGen/py_server.py
@@ -3,9 +3,9 @@ from flask import request
 from . import room_testing
 import schedule
 import time
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session,sessionmaker
-from zope.sqlalchemy import ZopeTransactionExtension
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import scoped_session,sessionmaker
+# from zope.sqlalchemy import ZopeTransactionExtension
 
 #make a connection to DB
 
