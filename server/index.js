1d8c378e204071109e9dfbcb67dd58fe22198e93
kiwook <kiwookiwook@gmail.com>
2017-11-03 00:27:27 -0700

    io.sockets to emit before socket connection + minor bug fixes

--- server/index.js
+++ server/index.js
@@ -9,8 +9,8 @@ const app = express();
 const fs = require('fs');
 const https = require('https');
 const http = require('http');
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const server = http.Server(app);
+const io = require('socket.io')(server)
 require('../db/models/dataModels')
 
 const PORT = 3000;
@@ -25,7 +25,7 @@ class RoomGen {
     this.userIds.push(userId);
   }
   isPopulated() {
-    return this.userIds.length === srms.length
+    return this.userIds.length === this.srms.length
   }
 } 
 
@@ -48,48 +48,48 @@ app.post('/flask', (req, res) => {
   const tempRoomParsed = JSON.parse(tempRoom.room)
 
   // put room instantiation in if (tempRoom.length === #)
-  room = new RoomGen(tempRoomParsed)
+  room = new RoomGen(tempRoomParsed);
+  io.sockets.emit('roomReady', room)
   console.log ('server room', room)
   // io.sockets.emit('ready', { room, id });
   res.end();
 })
 
-// eventually ..
-  // Put socket events in if (!processing && room)
-
-
 io.on('connection', (socket) => {
   console.log('socket connected');
+  //if we have a room
+    //emit roomMade
+
   // hope there's no scoping issues
   // const { roomId } = socket.handshake.query || 'default';
   // socket.join(roomId);
 
-  if (room !== undefined) {
-    socket.on('inHolding', userId => {
-      room.forEach(person => {
-        console.log('COMPARING:', person[0], ' AGAINST', userId );
-        if (person[0] === userId) {     //person[0] = userId
-          room.receiveUserId(userId);
-          socket.emit('readyWaiting', room)
-        }
-      });
-      if (room.isPopulated()) {
-        room.userIds.forEach(user => {
-          if (user[1][0] === 'm') {     //person[1] = userSex
-            let tempRoom = [];
-            for (let i = 0; i < room.length / 2; i++) {
-              let vidRoomName = user[0] + '-' + i;
-              socket.join(vidRoomName);
-              console.log('roommade for male user', vidRoomName)
-              tempRoom.push(vidRoomName);
-            }
-            room.vidRooms.push(tempRoom);
-          }
-        })
-        console.log('room', room.vidRooms)
-        // for each female
-          // emit a row of vidRooms
+  socket.on('inHolding', userId => {
+    room.srms.forEach(person => {
+      console.log('COMPARING:', person[0], ' AGAINST', userId );
+      if (person[0] === userId) {     //person[0] = userId
+        room.receiveUserId(userId);
+        socket.emit('readyWaiting', room)
       }
     });
-  }
-});
+    if (room.isPopulated()) {
+      room.userIds.forEach(user => {
+        if (user[1][0] === 'm') {     //person[1] = userSex
+          let tempRoom = [];
+          for (let i = 0; i < room.length / 2; i++) {
+            let vidRoomName = user[0] + '-' + i;
+            socket.join(vidRoomName);
+            console.log('roommade for male user', vidRoomName)
+            tempRoom.push(vidRoomName);
+          }
+          room.vidRooms.push(tempRoom);
+        }
+      })
+      console.log('room', room.vidRooms)
+      // for each female
+        // emit a row of vidRooms
+    }
+  });
+})
+// eventually ..
+  // Put socket events in if (!processing && room)
